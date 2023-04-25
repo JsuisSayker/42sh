2c14bd3c674d8897cb9995b03913c08905a96a7f
Karu <arthur.lassausaie@epitech.eu>
2023-04-25 16:55:06 +0200

    [FIX] fixing coding style

--- src/command/alias.c
+++ src/command/alias.c
@@ -11,6 +11,18 @@
 #include "proto.h"
 #include "proto_lib.h"
 
+static int append_parameters_in_file(char *tmp_pwd, char **tab)
+{
+    char *line = my_strcat(tab[1], " ");
+    char *line_final = my_strcat(line, tab[2]);
+    free(line);
+    char *line_final_aze = my_strcat(line_final, "\n");
+    free(line_final);
+    append_str_to_file(tmp_pwd, line_final_aze);
+    free(tmp_pwd);
+    return OK;
+}
+
 int alias(base_minishell_t *base, char **tab)
 {
     if (tab == NULL)
@@ -19,25 +31,16 @@ int alias(base_minishell_t *base, char **tab)
     if (tmp_pwd == NULL)
         return KO;
     switch (my_tablen(tab)){
-    case 1:
-        display_file(tmp_pwd);
-        free(tmp_pwd);
-        break;
-    case 2:
-        display_file(tmp_pwd);
-        free(tmp_pwd);
-        break;
-    case 3:
-        char *line = my_strcat(tab[1], " ");
-        char *line_final = my_strcat(line, tab[2]);
-        free(line);
-        char *line_final_aze = my_strcat(line_final, "\n");
-        free(line_final);
-        append_str_to_file(tmp_pwd, line_final_aze);
-        free(tmp_pwd);
-        break;
-    default:
-        return 0;
+        case 1:
+        case 2:
+            display_file(tmp_pwd);
+            free(tmp_pwd);
+            break;
+        case 3:
+            append_parameters_in_file(tmp_pwd, tab);
+            break;
+        default:
+            break;
     }
     return OK;
 }
