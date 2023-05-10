dddc1a413b10aaf830475618c17684299b052a22
Tom Mendy <tom.mendy@epitech.eu>
2023-05-10 13:15:54 +0200

    [CS] fix error

--- src/command/file_function/redirector.c
+++ src/command/file_function/redirector.c
@@ -13,8 +13,8 @@
 #include "macro.h"
 #include "proto_lib.h"
 
-static file_function_sub(base_minishell_t *base, need_tab_t *need_tab,
-char **tab_command, int *x)
+static int file_function_sub(base_minishell_t *base, need_tab_t *need_tab,
+char **tab_command)
 {
     if (need_tab->redirect_arg == 2) {
         need_tab->redirect_arg = 0;
@@ -33,7 +33,7 @@ int file_function(base_minishell_t *base, need_tab_t *need_tab, int *x)
         return KO;
     tab_command = my_splitstr(base->p_command[need_tab->tab_pos_y]\
     [need_tab->tab_pos_x], ' ');
-    if (file_function_sub(base, need_tab, tab_command, x) == OK)
+    if (file_function_sub(base, need_tab, tab_command) == OK)
         return OK;
     if (need_tab->redirect_arg == 1) {
         base->yes_or_not = 0;
